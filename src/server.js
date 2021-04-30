54a0568d599cc5ef048753a005f595cc02be6a62
GentritBegaj <gentritbegaj@gmail.com>
2021-04-30 09:43:12 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+import productsRoutes from "./products/index.js";
+import usersRoutes from "./users/index.js";
+import {
+  badRequestErrorHandler,
+  notFoundErrorHandler,
+  catchAllErrorHandler,
+} from "./errorHandlers.js";
+
+const server = express();
+const port = process.env.PORT;
+
+server.use(cors());
+server.use(express.json());
+server.use("/products", productsRoutes);
+server.use("/users", usersRoutes);
+
+console.log(listEndpoints(server));
+
+server.use(badRequestErrorHandler);
+server.use(notFoundErrorHandler);
+server.use(catchAllErrorHandler);
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(
+    server.listen(port, () => console.log(`Server is running on port ${port}`))
+  )
+  .catch((error) => console.log(error));
