5fbbf34434fc887f4f427dfdf9183e309fc54d49
alihfph <ali.hfph@gmail.com>
2021-04-30 15:35:29 +0200

    photo

--- src/users/index.js
+++ src/users/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import UserModel from "./schema.js";
-// import UploadModel from "./uploadschema.js";
+import UploadModel from "./uploadschema.js";
 import q2m from "query-to-mongo";
-// import cloudinary from "cloudinary";
-// import formidable from "formidable"
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 } from "cloudinary";
+import multer from "multer";
+import { Result } from "express-validator";
 const router = Router();
 // cloudinary.config({
 //   cloud_name: process.env.CLOUD_NAME,
@@ -11,14 +13,68 @@ const router = Router();
 //   api_secret: process.env.CLOUD_API_SECRET,
 // });
 
-// router.post("/uploadUser", async (res, req, next) => {
-//   const userImage = new UploadModel(req.body);
-//   console.log(userImage);
-//   const result = await cloudinary.v2.uploader(req.file.path);
-//   userImage.imgURL = result.secure_url;
-//   await userImage.save();
-//   res.send({ message: "Image is uploaded" });
-// });
+const cloudinaryStorage = new CloudinaryStorage({
+  cloudinary: v2,
+  params: {
+    folder: "strive",
+  },
+});
+
+const uploader = multer({ storage: cloudinaryStorage });
+console.log(uploader);
+router.post("/upload", uploader.single("cover"), async (req, res, next) => {
+  try {
+    const newPic = req.file.path;
+
+    const photoModel = new UploadModel();
+    photoModel.imgURL = newPic;
+    console.log(newPic);
+    await photoModel.save();
+    res.send(req.file.path);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/upload", async (req, res, next) => {
+  try {
+    const users = await UploadModel.find();
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/upload/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const user = await UploadModel.findById(id);
+    if (user) {
+      res.send(user);
+    } else {
+      const error = new Error();
+      error.httpStatusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    console.log(error);
+    next("While reading users list a problem occurred!");
+  }
+});
+
+router.delete("/upload/:id", async (req, res, next) => {
+  try {
+    const user = await UploadModel.findByIdAndDelete(req.params.id);
+    if (user) {
+      res.send({ message: "photo has been deleted from DB" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 router.post("/", async (req, res, next) => {
   try {
